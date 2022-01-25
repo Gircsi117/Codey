2ce84956bedb16bde62a95745a14d650f86c7fb1
Gircsi117 <gilianerik1107@gmail.com>
2022-01-25 09:35:00 +0100

    Menü elkészítése angularral + menu design

--- Dokumentumok/Design/HTML/js/code.js
+++ Dokumentumok/Design/HTML/js/code.js
@@ -2,3 +2,27 @@ document.getElementById("waterRange").addEventListener("change", ()=>{
     //console.log($("#waterRange").val());
     $("#water-text").html(`Víz mennyiség: ${$("#waterRange").val()}dl`)
 })
+
+var app = angular.module("CodyApp", []);
+
+app.run(function ($rootScope) {
+    
+})
+
+app.controller("menuCtrl", function ($scope) {
+    $scope.nev = "Cody";
+    $scope.eszkozok = [
+        {name:"Cél meghatározása", url:"#"},
+        {name:"Étkezési napló", url:"#"},
+        {name:"Grafikonok", url:"#"},
+        {name:"Kalória táblázat", url:"#"},
+        {name:"Saját ételek", url:"#"},
+        {name:"Beállítások", url:"#"}
+    ];
+    $scope.info = [
+        {name:"Blog", url:"#"},
+        {name:"GYIK", url:"#"},
+        {name:"Kalóriaszámlálásról", url:"#"},
+        {name:"Rólunk", url:"#"}
+    ];
+})
