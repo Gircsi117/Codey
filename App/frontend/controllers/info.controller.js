0dad168ec008c40d610a79161cc4e182ce8efc2d
Pető Bálint László <petobalint09@gmail.com>
2022-03-14 16:35:04 +0100

    COD-64 ticket TODO kivételével kész - Függvényekre vár

--- App/frontend/controllers/info.controller.js
+++ App/frontend/controllers/info.controller.js
@@ -1,54 +1,54 @@
-const { default: axios } = require("axios");
+const { default: axios } = require('axios');
 
-exports.getBlogPage = async (req, res)=>{
+exports.getBlogPage = async (req, res) => {
     res.render('info/blogok');
-}
+};
 
-exports.getGyakoriPage = async (req, res)=>{
-    res.render('info/gyik');
-}
+exports.getGyakoriPage = async (req, res) => {
+    res.render('info/gyik', { cim: 'Gyakran ismételt kérdések', jog: req.session.user.jogosultsag });
+};
 
-exports.getKaloriarolPage = async (req, res)=>{
-    res.render('info/kaloriarol');
-}
+exports.getKaloriarolPage = async (req, res) => {
+    res.render('info/kaloriarol', { cim: 'Kalóriaszámlálásról', jog: req.session.user.jogosultsag });
+};
 
-exports.getRolunkPage = async (req, res)=>{
+exports.getRolunkPage = async (req, res) => {
     res.render('info/rolunk');
-}
+};
 
-exports.postBlog = async (req, res)=>{
+exports.postBlog = async (req, res) => {
     const { cim, tartalom, idopont } = req.body;
     const felhasznalo_id = req.session.user.id;
 
     axios({
-        method:'POST',
-        url:"http://localhost:3001/blog/postBlog",
-        headers: {apisecret: 123},
-        data:{
+        method: 'POST',
+        url: 'http://localhost:3001/blog/postBlog',
+        headers: { apisecret: 123 },
+        data: {
             felhasznalo_id,
             cim,
             tartalom,
-            idopont
-        }
+            idopont,
+        },
     })
-    .then((response)=>{
-        res.send({data: response.data});
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-}
+        .then((response) => {
+            res.send({ data: response.data });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
 
-exports.getAllActiveBlog = async (req, res)=>{
+exports.getAllActiveBlog = async (req, res) => {
     axios({
-        method:'GET',
-        url:"http://localhost:3001/blog/getAllActiveBlog",
-        headers: {apisecret: 123}
+        method: 'GET',
+        url: 'http://localhost:3001/blog/getAllActiveBlog',
+        headers: { apisecret: 123 },
     })
-    .then((response)=>{
-        res.send({data: response.data});
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-}
+        .then((response) => {
+            res.send({ data: response.data });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
