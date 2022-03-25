dc9876927e2a0a874b85e204fbccd1bf4cb97b76
Pető Bálint László <76950363+Ramenslurper69@users.noreply.github.com>
2022-03-25 23:02:17 +0100

    Revert "Merge branch 'main' into COD-56-informacio"
    This reverts commit 68c5de1ad3d7e90a5cae1190dbb3c8c3e3d4117d, reversing
    changes made to 69ece8cad673c2f53f3fc9f86e5d2c95f2055a6a.

--- App/_API/controllers/blog.controller.js
+++ App/_API/controllers/blog.controller.js
@@ -29,48 +29,47 @@ exports.postGetBlogByUser = async (req, res) => {
     }
 };
 
-
 exports.getAllActiveBlog = async (req, res) => {
-  try {
-    const blogs = await Blog.findAll({ where: { status: 1 } });
+    try {
+        const blogs = await Blog.findAll({ where: { status: 1 } });
 
-    if (!blogs) return res.send({ success: false });
+        if (!blogs) return res.send({ success: false });
 
-    let blogsWithName = [];
+        let blogsWithName = [];
 
-    for await (const blog of blogs) {
-      const user = await User.findOne({ where: { id: blog.felhasznalo_id } });
-      blogsWithName.push({
-        id: blog.id,
-        cim: blog.cim,
-        tartalom: blog.tartalom,
-        felhazsnalo: user.nev,
-        idopont: blog.idopont,
-      });
+        for await (const blog of blogs) {
+            const user = await User.findOne({ where: { id: blog.felhasznalo_id } });
+            blogsWithName.push({
+                id: blog.id,
+                cim: blog.cim,
+                tartalom: blog.tartalom,
+                felhasznalo: user.nev,
+                idopont: blog.idopont,
+            });
+        }
+
+        return res.send({ success: true, blogs: blogsWithName });
+    } catch (error) {
+        console.log(error);
     }
-
-    return res.send({ success: true, blogs: blogsWithName });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 exports.postBlog = async (req, res) => {
-  try {
-    const { felhasznalo_id, cim, tartalom, idopont } = req.body;
+    try {
+        const { felhasznalo_id, cim, tartalom, idopont } = req.body;
 
-    const blog = await Blog.create({
-      cim: cim,
-      tartalom: tartalom,
-      felhasznalo_id: felhasznalo_id,
-      idopont: idopont,
-      status: 0,
-    });
+        const blog = await Blog.create({
+            cim: cim,
+            tartalom: tartalom,
+            felhasznalo_id: felhasznalo_id,
+            idopont: idopont,
+            status: 0,
+        });
 
-    if (!blog) return res.send({ success: false });
+        if (!blog) return res.send({ success: false });
 
-    return res.send({ success: true });
-  } catch (error) {
-    console.log(error);
-  }
+        return res.send({ success: true });
+    } catch (error) {
+        console.log(error);
+    }
 };
